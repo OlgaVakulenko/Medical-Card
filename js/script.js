3d39595fd6e4812ea8bda63ed265896273204ca9
Fetisova Tetiana <tbabich3003@gmail.com>
2019-08-22 15:27:27 +0300

    attempts to use drag events

--- js/script.js
+++ js/script.js
@@ -44,6 +44,7 @@ function checkVisits(visits) {
 function pushVisitsToLocalStorage(visits) {
     if(visits.length>0){
         let localStorageVisits = JSON.stringify(visits);
+        alert(localStorageVisits);
         localStorage.setItem('localVisits',localStorageVisits);
     }else{
         localStorage.clear();
@@ -53,9 +54,9 @@ window.onload = checkVisits(visits);
 window.onload = function(){
     checkLocalStorage();
 };
-window.ondragstart = (e) => {
-    e.preventDefault()
-};
+// window.ondragstart = (e) => {
+//     e.preventDefault()
+// };
 
 class Visit {
     constructor(doctor, visitDate, fullName, visitTarget, visitID, comments) {
@@ -82,6 +83,7 @@ class Visit {
              visitField = this._p.cloneNode();
 
         this._newCard.setAttribute('data-visitId', this._visitId);
+        this._newCard.setAttribute('draggable', 'true');
         this._newCard.className = 'visiting-card';
         this._showMoreButton.className = 'show-more ';
         this._span.className = 'close';
@@ -104,57 +106,10 @@ class Visit {
 
     dragManager(){
         let card = this._newCard;
+        let board = this._board;
+        let dragSrcEl = null;
+
         console.log(card);
-        card.onmousedown = function(e) {
-
-            let coords = getCoords(card);
-            let shiftX = e.pageX - coords.left;
-            let shiftY = e.pageY - coords.top;
-
-            card.style.position = 'absolute';
-            document.body.appendChild(card);
-            moveAt(e);
-
-            card.style.zIndex = '10';
-
-            function moveAt(e) {
-                card.style.left = e.pageX - shiftX + 'px';
-                card.style.top = e.pageY - shiftY + 'px';
-                // const cardContainerSize = document.querySelector('.board-container').getBoundingClientRect();
-                // const cardSize = card.getBoundingClientRect();
-                //
-                //     if(cardContainerSize.top <= cardSize.top){
-                //         card.style.left = e.pageX - shiftX + 'px';
-                //         card.style.top = e.pageY - shiftY + 'px';
-                //     }
-                //     else{
-                //         card.style.top = cardContainerSize.top  + 'px';
-                //     }
-                //
-                // if(cardContainerSize.bottom <= cardSize.bottom){
-                //     card.style.top = e.pageY - shiftY + 'px';
-                // }
-                // else{
-                //     card.style.bottom = cardContainerSize.bottom  + 'px';
-                // }
-            }
-
-            document.onmousemove = function(e) {
-                moveAt(e);
-            };
-
-            card.onmouseup = function() {
-                document.onmousemove = null;
-                card.onmouseup = null;
-            };
-
-        };
-
-        card.ondragstart = function() {
-
-            return false;
-        };
-
         function getCoords(elem) {
             let box = elem.getBoundingClientRect();
             return {
@@ -162,6 +117,139 @@ class Visit {
                 left: box.left + pageXOffset
             };
         }
+        function moveAt(e) {
+            card.style.left = e.pageX - shiftX + 'px';
+            card.style.top = e.pageY - shiftY + 'px';
+        }
+        function handleDragStart(e) {
+            // this.style.opacity = '0.4';
+            dragSrcEl = this;
+
+            e.dataTransfer.effectAllowed = 'move';
+            let coords = getCoords(card);
+                let shiftX = e.pageX - coords.left;
+                let shiftY = e.pageY - coords.top;
+
+                card.style.position = 'absolute';
+                document.body.appendChild(card);
+                moveAt(e);
+
+                card.style.zIndex = '10';
+
+
+                    // const cardContainerSize = document.querySelector('.board-container').getBoundingClientRect();
+                    // const cardSize = card.getBoundingClientRect();
+                    //
+                    //     if(cardContainerSize.top <= cardSize.top){
+                    //         card.style.left = e.pageX - shiftX + 'px';
+                    //         card.style.top = e.pageY - shiftY + 'px';
+                    //     }
+                    //     else{
+                    //         card.style.top = cardContainerSize.top  + 'px';
+                    //     }
+                    //
+                    // if(cardContainerSize.bottom <= cardSize.bottom){
+                    //     card.style.top = e.pageY - shiftY + 'px';
+                    // }
+                    // else{
+                    //     card.style.bottom = cardContainerSize.bottom  + 'px';
+                    // }
+                }
+            // e.dataTransfer.setData('text/html', this.innerHTML);
+
+        function handleDragOver(e) {
+            if (e.preventDefault) {
+                e.preventDefault(); // Necessary. Allows us to drop.
+            }
+
+            e.dataTransfer.dropEffect = 'move';  // See the section on the DataTransfer object.
+
+            return false;
+        }
+        function handleDrop(e) {
+            // this / e.target is current target element.
+
+            if (e.stopPropagation) {
+                e.stopPropagation(); // stops the browser from redirecting.
+            }
+            if (dragSrcEl !== this) {
+                // Set the source column's HTML to the HTML of the column we dropped on.
+                dragSrcEl.innerHTML = this.innerHTML;
+                this.innerHTML = e.dataTransfer.getData('text/html');
+            }
+            this.style.opacity = '1';
+
+            // See the section on the DataTransfer object.
+
+            return false;
+        }
+
+
+        card.addEventListener('dragstart', handleDragStart, false);
+        board.addEventListener('dragover', handleDragOver, false);
+        card.addEventListener('drop', handleDrop, false);
+
+
+
+
+
+        // card.onmousedown = function(e) {
+        //
+        //     let coords = getCoords(card);
+        //     let shiftX = e.pageX - coords.left;
+        //     let shiftY = e.pageY - coords.top;
+        //
+        //     card.style.position = 'absolute';
+        //     document.body.appendChild(card);
+        //     moveAt(e);
+        //
+        //     card.style.zIndex = '10';
+        //
+        //     function moveAt(e) {
+        //         card.style.left = e.pageX - shiftX + 'px';
+        //         card.style.top = e.pageY - shiftY + 'px';
+        //         // const cardContainerSize = document.querySelector('.board-container').getBoundingClientRect();
+        //         // const cardSize = card.getBoundingClientRect();
+        //         //
+        //         //     if(cardContainerSize.top <= cardSize.top){
+        //         //         card.style.left = e.pageX - shiftX + 'px';
+        //         //         card.style.top = e.pageY - shiftY + 'px';
+        //         //     }
+        //         //     else{
+        //         //         card.style.top = cardContainerSize.top  + 'px';
+        //         //     }
+        //         //
+        //         // if(cardContainerSize.bottom <= cardSize.bottom){
+        //         //     card.style.top = e.pageY - shiftY + 'px';
+        //         // }
+        //         // else{
+        //         //     card.style.bottom = cardContainerSize.bottom  + 'px';
+        //         // }
+        //     }
+        //
+        //     document.onmousemove = function(e) {
+        //         moveAt(e);
+        //     };
+        //
+        //     card.onmouseup = function() {
+        //         document.onmousemove = null;
+        //         card.onmouseup = null;
+        //     };
+        //
+        // };
+        //
+        // card.ondragstart = function() {
+        //
+        //     return false;
+        // };
+        //
+        // function getCoords(elem) {
+        //     let box = elem.getBoundingClientRect();
+        //     return {
+        //         top: box.top + pageYOffset,
+        //         left: box.left + pageXOffset
+        //     };
+        // }
     }
 }
 
@@ -274,7 +362,7 @@ function checkLocalStorage() {
                     savedVisit.showMore();
                     break;
             }
-
+            savedVisit.dragManager();
             addVisit(savedVisit);
             console.log(savedVisit);
             const closeCards = document.querySelectorAll('.close');
@@ -376,25 +464,34 @@ modalButton.addEventListener('click', function (e) {
     }
 
     addVisit(newVisit);
+
     console.log(newVisit);
     const closeCards = document.querySelectorAll('.close');
+    console.log('closeCard',closeCards);
     closeCards.forEach((closeCard)=>
         closeCard.onclick = function(e){
+            console.log('closeCard onclick applied');
         removeVisit(e)
     }
     );
     checkVisits(visits);
     modalWindow.reset();
     modalWindow.classList.remove('active');
+    newVisit.dragManager();
 });
 function removeVisit(e) {
+    console.log('function remove Visit applied');
     let visitingCardID = e.target.parentNode.parentNode.dataset.visitid;
+    console.log('card ID to remove: ', visitingCardID);
     let visitObjToRemove= document.querySelector(`.visiting-card[data-visitid="${visitingCardID}"]`);
+    console.log('card in DOM to remove ', visitObjToRemove);
     let removeIndex = visits.findIndex((e)=>{
-        return e.visitId === visitingCardID;
+        return e.visitId === +visitingCardID;
 
     });
+    console.log('index in array to remove',removeIndex);
     visits.splice(removeIndex, 1);
+    console.log('visits Array after Remove Visits func',visits);
     checkVisits(visits);
     visitObjToRemove.remove();
 }
